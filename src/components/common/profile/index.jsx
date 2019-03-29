32aa909192a5c88f514bdd401e38fc1b25928f3c
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-03-30 03:54:15 +0500

    Update: :construction: UI of Home and remove header

---
+++ src/components/common/profile/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const Profile = props => {
+  return <p>Profile</p>;
+};
+
+export default Profile;
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 // import Login from "../login";
 import "./style.scss";
-import Header from "../common/header";
+import Profile from "../common/profile";
 class Home extends Component {
   constructor(props) {
     super(props);
@@ -11,7 +11,73 @@ class Home extends Component {
     return (
       <div className="home">
         {/* <Login /> */}
-        <Header />
+        <div className="flex-container">
+          <div className="item">
+            <div className="flex-container">
+              {/* Header */}
+              <div className="item">
+                <h2>CONNECT</h2>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="flex-container">
+              {/* Header */}
+              <div className="item">
+                <div className="flex-container search">
+                  <i className="fas fa-search item" />
+                  <input
+                    type="text "
+                    className="item"
+                    placeholder="Search Here"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="flex-container">
+              {/* Header */}
+              <div className="item">
+                <div className="profile flex-container">
+                  <div className="item">
+                    <img src="/assets/images/1.jpeg" alt="" />
+                  </div>
+                  <div className="item">
+                    <h4>Hams Ahmed Ansari</h4>
+                    <p>
+                      <i className="fas fa-dot-circle" /> Online
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div className="header flex-container">
+          <div className="item">
+            <h2 className="logo">CONNECT</h2>
+          </div>
+          <div className="item">
+            <div className="flex-container search">
+              <i className="fas fa-search item" />
+              <input type="text " className="item" placeholder="Search Here" />
+            </div>
+          </div>
+          <div className="item">
+            <div className="profile flex-container">
+              <div className="item">
+                <img src="/assets/images/1.jpeg" alt="" />
+              </div>
+              <div className="item">
+                <h4>Hams Ahmed Ansari</h4>
+                <p>
+                  <i className="fas fa-dot-circle" /> Online
+                </p>
+              </div>
+            </div>
+          </div>
+        </div> */}
       </div>
     );
   }
