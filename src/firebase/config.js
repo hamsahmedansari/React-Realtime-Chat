3fe1cfb83d6a49776c2d9a4549b084b3ef42e98d
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-04-10 16:24:24 +0500

    DEPLOY: :rocket: perparing for deplyoment

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyCspseAH2VQNpWNHFCSSNCf6bvLgPG5sm0",
+  authDomain: "react-chat-3cb5c.firebaseapp.com",
+  databaseURL: "https://react-chat-3cb5c.firebaseio.com",
+  projectId: "react-chat-3cb5c",
+  storageBucket: "react-chat-3cb5c.appspot.com",
+  messagingSenderId: "443045259344"
+};
