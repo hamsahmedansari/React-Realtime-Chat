70a048bcc251a9fb4ef9bf26cc12f77cd40c4477
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-04-14 17:12:09 +0500

    UPDATE: :construction: notification new message scroll

--- src/components/common/message/index.jsx
+++ src/components/common/message/index.jsx
@@ -6,60 +6,146 @@ import GetDate from "../../../common/getDate";
 import { compose } from "redux";
 import { firestoreConnect } from "react-redux-firebase";
 
-const Message = props => {
-  // return (  );
-  const { sibling, chat, users, uid, firestore, roomId, chatId } = props;
-  // console.log(props);
-
-  if (!chat) return false;
-
-  const { createdBy, createAt, isSeen, message } = chat;
-  const currentUsers = users ? users.find(a => a.uid === createdBy) : null;
-  const mine = currentUsers.uid === uid ? true : false;
-
-  if (!mine) {
-    if (!isSeen) {
-      firestore
-        .collection("chatRooms")
-        .doc(roomId)
-        .collection("messages")
-        .doc(chatId)
-        .set(
-          {
-            isSeen: true
-          },
-          { merge: true }
-        );
-    }
+class Message extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
   }
+  componentDidMount() {
+    this.refs.message.scrollIntoView();
+  }
+  render() {
+    const { sibling, chat, users, uid, firestore, roomId, chatId } = this.props;
+    // console.log(props);
 
-  return (
-    // secondMessage
-    <div
-      className={`message flex-container ${sibling ? "secondMessage" : ""} ${
-        mine ? "mine" : ""
-      }`}
-    >
-      {!mine && <img src={currentUsers.image} alt="" className="item" />}
-      <div className="item message-body">
-        <p>
-          {String(message)
-            .slice(0, 1)
-            .toUpperCase()
-            .concat(String(message).slice(1))}
-        </p>
-        <p>
-          <span>
-            {mine && <strong>{isSeen ? "Seen at" : "Delivered at"}</strong>}{" "}
-            &nbsp;
-          </span>
-          {GetDate(createAt)} ago
-        </p>
-        <p>{/* <strong>Message Not Send</strong> */}</p>
+    if (!chat) return false;
+
+    const { createdBy, createAt, isSeen, message } = chat;
+    const currentUsers = users ? users.find(a => a.uid === createdBy) : null;
+    const mine = currentUsers.uid === uid ? true : false;
+
+    if (!mine) {
+      if (!isSeen) {
+        firestore
+          .collection("chatRooms")
+          .doc(roomId)
+          .collection("messages")
+          .doc(chatId)
+          .set(
+            {
+              isSeen: true
+            },
+            { merge: true }
+          );
+      }
+    }
+
+    return (
+      // secondMessage
+      <div
+        className={`message flex-container ${sibling ? "secondMessage" : ""} ${
+          mine ? "mine" : ""
+        }`}
+        ref="message"
+      >
+        {!mine && (
+          <img
+            src={currentUsers.image}
+            alt=""
+            className={`item animated faster flipInX}`}
+          />
+        )}
+        <div
+          className={`item message-body ${
+            mine ? " animated faster fadeInRight" : "animated faster flipInY"
+          }`}
+        >
+          <p>
+            {String(message)
+              .slice(0, 1)
+              .toUpperCase()
+              .concat(String(message).slice(1))}
+          </p>
+          <p>
+            <span>
+              {mine && <strong>{isSeen ? "Seen at" : "Delivered at"}</strong>}{" "}
+              &nbsp;
+            </span>
+            {GetDate(createAt)} ago
+          </p>
+          <p>{/* <strong>Message Not Send</strong> */}</p>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// export default Message;
+
+// const Message = props => {
+//   // return (  );
+//   const { sibling, chat, users, uid, firestore, roomId, chatId } = props;
+//   // console.log(props);
+
+//   if (!chat) return false;
+
+//   const { createdBy, createAt, isSeen, message } = chat;
+//   const currentUsers = users ? users.find(a => a.uid === createdBy) : null;
+//   const mine = currentUsers.uid === uid ? true : false;
+
+//   if (!mine) {
+//     if (!isSeen) {
+//       firestore
+//         .collection("chatRooms")
+//         .doc(roomId)
+//         .collection("messages")
+//         .doc(chatId)
+//         .set(
+//           {
+//             isSeen: true
+//           },
+//           { merge: true }
+//         );
+//     }
+//   }
+
+//   return (
+//     // secondMessage
+//     <div
+//       className={`message flex-container ${sibling ? "secondMessage" : ""} ${
+//         mine ? "mine" : ""
+//       }`}
+//     >
+//       {!mine && (
+//         <img
+//           src={currentUsers.image}
+//           alt=""
+//           className={`item animated faster flipInX}`}
+//         />
+//       )}
+//       <div
+//         className={`item message-body ${
+//           mine ? " animated faster fadeInRight" : "animated faster flipInY"
+//         }`}
+//       >
+//         <p>
+//           {String(message)
+//             .slice(0, 1)
+//             .toUpperCase()
+//             .concat(String(message).slice(1))}
+//         </p>
+//         <p>
+//           <span>
+//             {mine && <strong>{isSeen ? "Seen at" : "Delivered at"}</strong>}{" "}
+//             &nbsp;
+//           </span>
+//           {GetDate(createAt)} ago
+//         </p>
+//         <p>{/* <strong>Message Not Send</strong> */}</p>
+//       </div>
+//     </div>
+//   );
+// };
 
 Message.propTypes = {
   sibling: PropTypes.bool
